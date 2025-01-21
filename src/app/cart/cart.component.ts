fb3add4141054cf36fa129dabd17d84c75c6be23
Adhavan-TechStar <adhavan.tsb@techstargroup.com>
2025-01-21 13:29:59 +0530

    without empty cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
 import { CartService, CartItem } from '../services/cart.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -12,28 +11,23 @@ import { FormsModule } from '@angular/forms';
 })
 export class CartComponent implements OnInit {
   cartItems$!: Observable<CartItem[]>;
-
+cartItems: any;
   constructor(private cartService: CartService) {}
-
   ngOnInit(): void {
     this.cartItems$ = this.cartService.getCartItems();
   }
-
   updateItemQuantity(item: CartItem, quantity: number): void {
     if (quantity > 0) {
       item.quantity = quantity;
       this.cartService.updateCartItem(item); // Call updateCartItem here
     }
   }
-
   removeItem(item: CartItem): void {
     this.cartService.removeItem(item.id); // Remove item by ID
   }
-
   calculateTotal(cartItems: CartItem[]): number {
     return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
   }
-
   checkout(): void {
     console.log('Proceeding to checkout...');
   }
